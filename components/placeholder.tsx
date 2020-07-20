90a4693c1f128ff1d70c8263981710191c42a8d5
Max Davitt <med112@zips.uakron.edu>
2020-07-19 23:59:35 -0400

    Rename import in placeholder.tsx
    WTF, why did it still work when I built it on my PC?

--- components/placeholder.tsx
+++ components/placeholder.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 
-import styles from './Placeholder.module.css';
+import styles from './placeholder.module.css';
 
 export const Placeholder: FunctionComponent = ({ children }) => (
 	<div className={styles.container}>
